d487b89fdbf0ea93ef5b93b191f54683e6ec8d04
Kevin Wang <kevmo314@gmail.com>
2021-03-07 20:41:50 -0500

    update transmuxer url

--- nginx-rtmp-srt-transmuxer/ffmpeg.sh
+++ nginx-rtmp-srt-transmuxer/ffmpeg.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH=/usr/local/lib
 
 echo $SRT_RELAY_SERVER
 
-/usr/local/bin/ffmpeg -i rtmp://localhost:1935/live/$1 -vcodec copy -pkt_size 1316 -f mpegts srt://${SRT_RELAY_SERVER}?streamid=input/live/$1 >> /dev/stdout 2>> /dev/stderr
+/usr/local/bin/ffmpeg -i rtmp://localhost:1935/live/$1 -vcodec copy -pkt_size 1316 -f mpegts srt://${SRT_RELAY_SERVER}?streamid=publish/$1 >> /dev/stdout 2>> /dev/stderr
